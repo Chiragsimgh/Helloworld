31a0e086035579594db95b5548087f96996cc615
Chirag <schirag937@gmail.com>
2022-03-12 10:54:08 +0530

    [Chirag]

---
+++ firstFile.sh
@@ -0,0 +1,22 @@
+#! /bin/bash -x
+
+yourName="Mayank"
+echo $yourName
+
+#loop in shell script
+for((i=0;i<10;i++))
+do
+#body of the loop
+echo $i
+done
+
+#while loop
+echo "=============*************============="
+num=10
+while [ $num -gt 0 ]
+do
+	#body of the loop
+	echo $num
+	((num--))
+
+done
